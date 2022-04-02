13238fb4d339c8e78bc1d1258e1ec40eb12536d0
CHAMS1110 <chamseddine-99@outlook.com>
2022-04-02 12:51:55 +0200

    file update

--- core/settings.py
+++ core/settings.py
@@ -16,11 +16,14 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+#ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS        = ['https://stm-101.herokuapp.com','127.0.0.1:8000']
+#CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+CSRF_TRUSTED_ORIGINS = ['https://stm-101.herokuapp.com','127.0.0.1:8000']
 
 # Application definition
 
